4270b721e515e7ffc74faf618976f4b32a57068f
Wang Wenxiang <wenxiang.wang@daolicloud.comm>
2016-09-17 20:25:04 +0800

    Add migrate container to change group and firewall.

--- api/container.go
+++ api/container.go
@@ -1,10 +1,15 @@
 package api
 
 import (
+    "encoding/json"
     "fmt"
     "net/http"
+    "path"
+    "strconv"
+    "strings"
 
     log "github.com/Sirupsen/logrus"
+    "github.com/daolinet/daolinet/model"
     "github.com/gorilla/mux"
     "github.com/samalba/dockerclient"
 )
@@ -102,4 +107,73 @@ func (a *Api) resetContainer(w http.ResponseWriter, r *http.Request) {
     }
 
     w.Write([]byte(newId))
+}
+
+func (a *Api) resetContainerById(oldId , newId string) {
+    // Firewall, Group, Policy
+    firewalls, err := a.store.List(pathNameFirewall)
+    if err != nil {
+        log.Errorf("error to get all firewalls: %v", err)
+    } else {
+        for _, fw := range firewalls {
+            var firewall model.Firewall
+            err := json.Unmarshal(fw.Value, &firewall)
+            if err != nil {
+                log.Errorf("error unmarshal firewall: %v", err)
+                continue
+            }
+            if firewall.Container == oldId {
+                firewall.Container = newId
+                value, err := json.Marshal(firewall)
+                if err != nil {
+                    log.Errorf("json marshal error: %v", err)
+                    continue
+                }
+                if err := a.store.Put(fw.Key, value, nil); err != nil {
+                    log.Errorf("error to update container firewall by name: %v", err)
+                    continue
+                }
+                nodeurl := path.Join(pathNodeFirewall, firewall.DatapathID, strconv.Itoa(firewall.GatewayPort))
+                if err := a.store.Put(nodeurl, value, nil); err != nil {
+                    log.Errorf("error to update container firewall by node: %v", err)
+                    continue
+                }
+            }
+        }
+    }
+
+    policies, err := a.store.List(pathPolicy)
+    if err != nil {
+        log.Errorf("error to get all policies : %v", err)
+    } else {
+        for _, policy := range policies {
+            peer := strings.Split(policy.Key, "/")
+            parts := strings.Split(peer[len(peer)-1], ":")
+            if len(parts) != 2 {
+                log.Error(ErrPolicyFormat.Error())
+                continue
+            }
+            if oldId == parts[0] || oldId == parts[1] {
+                if err := a.store.Delete(policy.Key); err != nil {
+                    log.Warnf("error deleting policy: %v", err)
+                }
+                if oldId == parts[0] {
+                    parts[0] = newId
+                } else {
+                    parts[1] = newId
+                }
+
+                var key string
+                if strings.Compare(parts[0], parts[1]) > 0 {
+                    key = fmt.Sprintf("%s:%s", parts[1], parts[0])
+                } else {
+                    key = fmt.Sprintf("%s:%s", parts[0], parts[1])
+                }
+
+                if err := a.store.Put(path.Join(pathPolicy, key), policy.Value, nil); err != nil {
+                    log.Errorf("error to save policy: %v", err)
+                }
+            }
+        }
+    }
 }
